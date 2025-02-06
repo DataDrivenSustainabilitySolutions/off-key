1fe190ee544cde7fb7c9724b357ed3dd02edcc85
Oliver Hennhöfer <oliver.hennhoefer@mail.de>
2025-02-06 13:42:17 +0100

    Adds /active and /active/id Routes

--- backend/app/api/v1/chargers.py
+++ backend/app/api/v1/chargers.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-#from backend.app.db.models import Chargers
+from backend.app.db.models import Chargers
 from backend.app.db.session import SessionLocal
 from backend.app.services.chargers_sync import ChargersSyncService
 
 router = APIRouter()
+
 
 # Dependency for database sessions
 def get_db():
@@ -15,19 +16,20 @@ def get_db():
     finally:
         db.close()
 
+
 @router.get("/sync")
 async def sync_chargers(db: Session = Depends(get_db)):
     service = ChargersSyncService(db)
     await service.sync_chargers()
     return {"status": "successful"}
 
-"""@router.get("/active")
+
+@router.get("/active")
 async def get_active_chargers(db: Session = Depends(get_db)):
-    return db.query(Chargers).filter(Chargers.online is True).all()
+    return db.query(Chargers).filter(Chargers.online).all()
+
 
 @router.get("/active/id")
 async def get_active_charger_ids(db: Session = Depends(get_db)):
-    active_ids = db.query(Chargers.charger_id) \
-        .filter(Chargers.online is True) \
-        .all()
-    return {"active": [active_id[1] for active_id in active_ids]}"""
+    active_ids = db.query(Chargers.charger_id).filter(Chargers.online).all()
+    return {"active": [active_id[0] for active_id in active_ids]}

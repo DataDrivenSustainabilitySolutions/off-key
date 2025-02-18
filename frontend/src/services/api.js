45caf1599b5a4f7825387c621f2afdd7cf060cc6
Oliver Hennhöfer <oliver.hennhoefer@mail.de>
2025-02-18 18:13:56 +0100

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Assuming the API URL is set in the environment variable
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // from .env file
+  baseURL: import.meta.env.VITE_API_URL, // Now this is http://localhost:8000
 });
 
 export const fetchData = async () => {

4b167e90a095975901799f794baf5380e3ca9575
homa1074 <marius.hoell@protonmail.com>
2025-05-15 14:51:49 +0200

    deleted

--- backend/src/api/v1/routes.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from . import charger, telemetry, auth
-
-router = APIRouter()
-
-router.include_router(auth.router, prefix="/auth", tags=["auth"])
-router.include_router(charger.router, prefix="/chargers", tags=["chargers"])
-router.include_router(telemetry.router, prefix="/telemetry", tags=["telemetry"])

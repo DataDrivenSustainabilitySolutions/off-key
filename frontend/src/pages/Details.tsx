3321d2bdac46f6a3185c01f490338adb5f128a07
Marco Catoir <cama1025@h-ka.de>
2025-05-23 16:38:23 +0200

    Datepicker for LineChart

--- frontend/src/pages/Details.tsx
+++ frontend/src/pages/Details.tsx
@@ -1,4 +1,5 @@
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import mockdata from "../mockData/MockDataDetails.json";
 import { useRedZones } from "../lib/useRedZones";
 import {
@@ -21,6 +22,9 @@ import {
 import { useEffect, useRef, useState } from "react";
 // import { useParams } from "react-router-dom";
 import axios from "axios";
+import { format } from "date-fns";
+import { Popover } from "@/components/ui/popover";
+
 // import {
 //   Table,
 //   TableBody,
@@ -47,17 +51,21 @@ const data = mockdata;
 //   { Day: "Saturday", UsageCount: 122 },
 // ];
 
+interface Cpu {
+  timestamp: string;
+  value: number;
+}
+
 const Details: React.FC = () => {
   const redZones = useRedZones(data, 80);
   const [collapsedCard, setCollapsedCard] = useState<Record<string, boolean>>({
     smallCard1: false,
     smallCard2: false,
     smallCard3: false,
-    CPUUsageCard: false,
-    CPUThermalCard: false,
+    CpuUsageCard: false,
+    CpuThermalCard: false,
   });
   const [, setSearchError] = useState(false);
-
   // Hardcoded for now; replace with useParams() when routing
   const charger_id = "d2d67b85-f56b-4a50-842d-92f210e77076";
 
@@ -66,13 +74,17 @@ const Details: React.FC = () => {
     controllertemperaturecpu_thermal,
     setControllertemperaturecpu_thermal,
   ] = useState<string>();
-  const [controllerCpuUsageData, setControllerCpuUsageData] = useState<
-    unknown[]
-  >([]);
+  const [fromDateUsage, setFromDateUsage] = useState<Date>();
+  const [fromDateThermal, setFromDateThermal] = useState<Date>();
+  const [toDateUsage, setToDateUsage] = useState<Date>();
+  const [toDateThermal, setToDateThermal] = useState<Date>();
+  const [controllerCpuUsageData, setControllerCpuUsageData] = useState<Cpu[]>(
+    []
+  );
   const [
     controllertemperaturecpu_thermalData,
     setControllertemperaturecpu_thermalData,
-  ] = useState<unknown[]>([]);
+  ] = useState<Cpu[]>([]);
   const intervalRef = useRef<ReturnType<typeof setInterval> | null>(null);
 
   // 1) only at mount or when charger_id is changing: get Telemetry types
@@ -131,14 +143,14 @@ const Details: React.FC = () => {
       try {
         // CPU Usage
         const respUsage = await axios.get(
-          `http://127.0.0.1:8000/v1/telemetry/${charger_id}/${controllerCpuUsage}?limit=50`
+          `http://127.0.0.1:8000/v1/telemetry/${charger_id}/${controllerCpuUsage}?limit=100`
         );
         setControllerCpuUsageData(respUsage.data);
         console.log("CPU Usage:", respUsage.data);
 
         // CPU Thermal
         const respThermal = await axios.get(
-          `http://127.0.0.1:8000/v1/telemetry/${charger_id}/${controllertemperaturecpu_thermal}?limit=50`
+          `http://127.0.0.1:8000/v1/telemetry/${charger_id}/${controllertemperaturecpu_thermal}?limit=100`
         );
         setControllertemperaturecpu_thermalData(respThermal.data);
         console.log("CPU Thermal:", respThermal.data);
@@ -177,10 +189,27 @@ const Details: React.FC = () => {
     return `${day}.${month}, ${hour}:${minute}:${second}`;
   };
 
+  //function to filter the Cpu Usage with the Dates the user selected in the Navbar for the Diagram
+  const filteredDataCpuUsage = controllerCpuUsageData.filter((d) => {
+    const t = new Date(d.timestamp).getTime();
+    const f = fromDateUsage?.getTime() ?? -Infinity;
+    const u = toDateUsage?.getTime() ?? Infinity;
+    return t >= f && t <= u;
+  });
+
+  const filteredDataCpuThermal = controllertemperaturecpu_thermalData.filter(
+    (d) => {
+      const t = new Date(d.timestamp).getTime();
+      const f = fromDateThermal?.getTime() ?? -Infinity;
+      const u = toDateThermal?.getTime() ?? Infinity;
+      return t >= f && t <= u;
+    }
+  );
+
   return (
     <div className="flex mt-5">
       <Card className="ml-16 bg-white shadow-md w-11/12 min-h-11/12">
-        <CardTitle className="ml-5">Charger 1</CardTitle>
+        <CardTitle className="ml-5">Charger {charger_id}</CardTitle>
         <CardContent>
           <div className="flex justify-between">
             {/* <Card
@@ -331,23 +360,79 @@ const Details: React.FC = () => {
           </div>
 
           <Card
-            className={`mr-6 w-full mb-4 transition-all duration-300 ${
-              collapsedCard.CPUUsageCard ? "h-16" : "h-96"
+            className={`mr-6 w-full mb-4 transition-all duration-300 overflow-hidden ${
+              collapsedCard.CpuUsageCard ? "h-16" : "h-96"
             }`}
           >
             <div className="flex justify-between">
-              <CardTitle className="ml-5">
-                CPU Usage of Charger {charger_id}
-              </CardTitle>
+              <CardTitle className="ml-5">CPU Usage</CardTitle>
+              {!collapsedCard.CpuUsageCard && (
+                <div className="flex">
+                  <Popover>
+                    <div className="relative">
+                      <Input
+                        type="Date"
+                        className="cursor-pointer"
+                        placeholder="Von"
+                        value={
+                          fromDateUsage
+                            ? format(fromDateUsage, "yyyy-MM-dd")
+                            : ""
+                        }
+                        onChange={(e) => {
+                          const value = e.target.value;
+                          setFromDateUsage(value ? new Date(value) : undefined);
+                        }}
+                      />
+                    </div>
+                  </Popover>
+                  <h2 className="ml-3 mr-3 text-2xl font-semibold tracking-tight transition-colors first:mt-0">
+                    :
+                  </h2>
+                  <Popover>
+                    <div className="relative">
+                      <Input
+                        type="Date"
+                        className="cursor-pointer"
+                        placeholder="Bis"
+                        value={
+                          toDateUsage ? format(toDateUsage, "yyyy-MM-dd") : ""
+                        }
+                        onChange={(e) => {
+                          const value = e.target.value;
+                          setToDateUsage(value ? new Date(value) : undefined);
+                        }}
+                      />
+                    </div>
+                  </Popover>
+                  <div>
+                    <div className="flex items-center h-9 ml-5 space-x-2 rounded-lg border bg-white px-3">
+                      <button
+                        // onClick={onLast8h}
+                        className="text-sm text-gray-700 hover:underline focus:outline-none"
+                      >
+                        Letzte 8 h
+                      </button>
+                      <div className="h-6 border-l border-gray-300 mx-2" />
+                      <button
+                        // onClick={onLast24h}
+                        className="text-sm text-gray-700 hover:underline focus:outline-none"
+                      >
+                        Letzte 24 h
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              )}
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                onClick={() => minimizeCards("CPUUsageCard")}
+                onClick={() => minimizeCards("CpuUsageCard")}
                 className={`size-6 mr-5 cursor-pointer transition-transform" ${
-                  collapsedCard.CPUUsageCard ? "rotate-180" : ""
+                  collapsedCard.CpuUsageCard ? "rotate-180" : ""
                 }`}
               >
                 <path
@@ -357,76 +442,139 @@ const Details: React.FC = () => {
                 />
               </svg>
             </div>
-            <ResponsiveContainer width="90%" height="90%">
-              <LineChart
-                width={500}
-                height={300}
-                data={controllerCpuUsageData}
-                margin={{
-                  top: 5,
-                  right: 30,
-                  left: 20,
-                  bottom: 5,
-                }}
-              >
-                <CartesianGrid strokeDasharray="5 5" />
-                <XAxis
-                  dataKey="timestamp"
-                  tickFormatter={formatDateMultiline}
-                />
-
-                <YAxis dataKey="value" />
-                <Tooltip />
-                <Legend />
-                {redZones.map((zone, index) => (
-                  <ReferenceArea
-                    key={index}
-                    x1={zone.start}
-                    x2={zone.end}
-                    strokeOpacity={0}
-                    fill="red"
-                    fillOpacity={0.1}
-                  />
-                ))}
-                <Line
-                  type="monotone"
-                  dataKey="value"
-                  stroke="#8884d8"
-                  activeDot={{ r: 8 }}
-                  dot={({ cx, cy, payload }) => {
-                    const color = payload.value >= 80 ? "red" : "#8884d8";
-                    return (
-                      <circle
-                        cx={cx}
-                        cy={cy}
-                        r={4}
-                        fill={color}
-                        stroke="none"
-                      />
-                    );
+            {!collapsedCard.CpuUsageCard && (
+              <ResponsiveContainer width="90%" height="90%">
+                <LineChart
+                  width={500}
+                  height={300}
+                  data={filteredDataCpuUsage.slice().reverse()}
+                  margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
                   }}
-                />
-              </LineChart>
-            </ResponsiveContainer>
+                >
+                  <CartesianGrid strokeDasharray="5 5" />
+                  <XAxis
+                    dataKey="timestamp"
+                    tickFormatter={formatDateMultiline}
+                  />
+
+                  <YAxis dataKey="value" />
+                  <Tooltip />
+                  <Legend />
+                  {redZones.map((zone, index) => (
+                    <ReferenceArea
+                      key={index}
+                      x1={zone.start}
+                      x2={zone.end}
+                      strokeOpacity={0}
+                      fill="red"
+                      fillOpacity={0.1}
+                    />
+                  ))}
+                  <Line
+                    type="monotone"
+                    dataKey="value"
+                    stroke="#8884d8"
+                    activeDot={{ r: 8 }}
+                    dot={({ cx, cy, payload }) => {
+                      const color = payload.value >= 80 ? "red" : "#8884d8";
+                      return (
+                        <circle
+                          key={`dot-${payload.timestamp}`}
+                          cx={cx}
+                          cy={cy}
+                          r={4}
+                          fill={color}
+                          stroke="none"
+                        />
+                      );
+                    }}
+                  />
+                </LineChart>
+              </ResponsiveContainer>
+            )}
           </Card>
           <Card
-            className={` w-full transition-all duration-300 ${
-              collapsedCard.CPUThermalCard ? "h-16" : "h-96"
+            className={` w-full transition-all duration-300 overflow-hidden ${
+              collapsedCard.CpuThermalCard ? "h-16" : "h-96"
             }`}
           >
             <div className="flex justify-between">
-              <CardTitle className="ml-5">
-                CPU Thermal of Charger {charger_id}
-              </CardTitle>
+              <CardTitle className="ml-5">CPU Thermal</CardTitle>
+              {!collapsedCard.CpuThermalCard && (
+                <div className="flex">
+                  <Popover>
+                    <div className="relative">
+                      <Input
+                        type="Date"
+                        className="cursor-pointer"
+                        placeholder="Von"
+                        value={
+                          fromDateThermal
+                            ? format(fromDateThermal, "yyyy-MM-dd")
+                            : ""
+                        }
+                        onChange={(e) => {
+                          const value = e.target.value;
+                          setFromDateThermal(
+                            value ? new Date(value) : undefined
+                          );
+                        }}
+                      />
+                    </div>
+                  </Popover>
+                  <h2 className="ml-3 mr-3 text-2xl font-semibold tracking-tight transition-colors first:mt-0">
+                    :
+                  </h2>
+                  <Popover>
+                    <div className="relative">
+                      <Input
+                        type="Date"
+                        className="cursor-pointer"
+                        placeholder="Bis"
+                        value={
+                          toDateThermal
+                            ? format(toDateThermal, "yyyy-MM-dd")
+                            : ""
+                        }
+                        onChange={(e) => {
+                          const value = e.target.value;
+                          setToDateThermal(value ? new Date(value) : undefined);
+                        }}
+                      />
+                    </div>
+                  </Popover>
+                  <div>
+                    <div className="flex items-center h-9 ml-5 space-x-2 rounded-lg border bg-white px-3">
+                      <button
+                        // onClick={onLast8h}
+                        className="text-sm text-gray-700 hover:underline focus:outline-none"
+                      >
+                        Letzte 8 h
+                      </button>
+                      <div className="h-6 border-l border-gray-300 mx-2" />
+                      <button
+                        // onClick={onLast24h}
+                        className="text-sm text-gray-700 hover:underline focus:outline-none"
+                      >
+                        Letzte 24 h
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              )}
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                onClick={() => minimizeCards("CPUThermalCard")}
+                onClick={() => minimizeCards("CpuThermalCard")}
                 className={`size-6 mr-5 cursor-pointer transition-transform" ${
-                  collapsedCard.CPUThermalCard ? "rotate-180" : ""
+                  collapsedCard.CpuThermalCard ? "rotate-180" : ""
                 }`}
               >
                 <path
@@ -436,51 +584,55 @@ const Details: React.FC = () => {
                 />
               </svg>
             </div>
-            <ResponsiveContainer width="90%" height="90%">
-              <LineChart
-                width={500}
-                height={300}
-                data={controllertemperaturecpu_thermalData}
-                margin={{
-                  top: 5,
-                  right: 30,
-                  left: 20,
-                  bottom: 5,
-                }}
-              >
-                <CartesianGrid strokeDasharray="5 5" />
-                <XAxis
-                  dataKey="timestamp"
-                  tickFormatter={formatDateMultiline}
-                />
-                <YAxis
-                  dataKey="value"
-                  domain={[30, 80]}
-                  ticks={[40, 50, 60, 70, 80]}
-                />
-                <Tooltip />
-                <Legend />
-                <Line
-                  type="monotone"
-                  dataKey="value"
-                  stroke="#8884d8"
-                  activeDot={{ r: 8 }}
-                  dot={({ cx, cy, payload }) => {
-                    //wenn value >= 59 dann roter punkt z.b.
-                    const color = payload.value >= 59 ? "red" : "#8884d8";
-                    return (
-                      <circle
-                        cx={cx}
-                        cy={cy}
-                        r={4}
-                        fill={color}
-                        stroke="none"
-                      />
-                    );
+
+            {!collapsedCard.CpuThermalCard && (
+              <ResponsiveContainer width="90%" height="90%">
+                <LineChart
+                  width={500}
+                  height={300}
+                  data={filteredDataCpuThermal.slice().reverse()}
+                  margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
                   }}
-                />
-              </LineChart>
-            </ResponsiveContainer>
+                >
+                  <CartesianGrid strokeDasharray="5 5" />
+                  <XAxis
+                    dataKey="timestamp"
+                    tickFormatter={formatDateMultiline}
+                  />
+                  <YAxis
+                    dataKey="value"
+                    domain={[30, 80]}
+                    ticks={[40, 50, 60, 70, 80]}
+                  />
+                  <Tooltip />
+                  <Legend />
+                  <Line
+                    type="monotone"
+                    dataKey="value"
+                    stroke="#8884d8"
+                    activeDot={{ r: 8 }}
+                    dot={({ cx, cy, payload }) => {
+                      //wenn value >= 59 dann roter punkt z.b.
+                      const color = payload.value >= 59 ? "red" : "#8884d8";
+                      return (
+                        <circle
+                          key={`dot-${payload.timestamp}`}
+                          cx={cx}
+                          cy={cy}
+                          r={4}
+                          fill={color}
+                          stroke="none"
+                        />
+                      );
+                    }}
+                  />
+                </LineChart>
+              </ResponsiveContainer>
+            )}
           </Card>
         </CardContent>
       </Card>

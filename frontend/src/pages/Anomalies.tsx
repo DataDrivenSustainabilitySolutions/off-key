73907f868417ea75792f50ceb1a1fd953a68c344
homa1074 <marius.hoell@protonmail.com>
2025-06-13 13:42:41 +0200

    added delete button

--- frontend/src/pages/Anomalies.tsx
+++ frontend/src/pages/Anomalies.tsx
@@ -1,5 +1,5 @@
 import { NavigationBar } from "@/components/NavigationBar";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import {
   Table,
   TableBody,
@@ -8,40 +8,56 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-
-type Anomaly = {
-  charger_id: string;
-  timestamp: string;
-  telemetry_type: string;
-  anomaly_type: string;
-};
+import { Link } from "react-router-dom";
+import { FetchContext } from "@/dataFetch/FetchContext";
+import { Anomaly } from "@/dataFetch/FetchContext";
 
 export default function AnomalyTable() {
   const [data, setData] = useState<Anomaly[]>([]);
   const [error, setError] = useState<string | null>(null);
+  const fetchContext = useContext(FetchContext);
+
+    const fetchAllAnomalies = async () => {
+    try {
+      if (!fetchContext) return;
+
+      const chargers = await fetchContext.getAllChargers();
+      const allAnomalies = await Promise.all(
+        chargers.map((charger) =>
+          fetchContext.getAnomalies(charger.charger_id)
+        )
+      );
+      const flattenedAnomalies = allAnomalies.flat();
+      setData(flattenedAnomalies);
+    } catch (err: any) {
+      setError(err.message || "Unknown error");
+    }
+  };
 
   useEffect(() => {
-    const fetchAnomalies = async () => {
-      try {
-        const res = await fetch(
-          "http://localhost:8000/v1/anomalies?charger_id=7939d75d-2ecc-40c3-aca2-fc6332873a4d"
-        );
-        if (!res.ok) throw new Error("Fehler beim Abrufen der Daten");
-        const json = await res.json();
-        setData(Array.isArray(json) ? json : []);
-      } catch (err: any) {
-        setError(err.message || "Unbekannter Fehler");
-      }
-    };
+    fetchAllAnomalies();
+  }, [fetchContext]);
 
-    fetchAnomalies();
-  }, []);
+  const handleDelete = async (anomaly: Anomaly) => {
+    if (!fetchContext) return;
+    try {
+      await fetchContext.deleteAnomaly(
+        anomaly.charger_id,
+        new Date(anomaly.timestamp),
+        anomaly.telemetry_type
+      );
+      
+      await fetchAllAnomalies();
+    } catch (err: any) {
+      setError("Error while deleting: " + err.message);
+    }
+  };
 
   return (
     <>
-      <NavigationBar></NavigationBar>
+      <NavigationBar />
       <div className="p-6">
-        <h1 className="text-xl font-bold mb-4">Anomalien</h1>
+        <h1 className="text-xl font-bold mb-4">Anomalies</h1>
 
         {error && <p className="text-red-500 mb-4">{error}</p>}
 
@@ -52,13 +68,22 @@ export default function AnomalyTable() {
               <TableHead>Timestamp</TableHead>
               <TableHead>Telemetry Type</TableHead>
               <TableHead>Anomaly Type</TableHead>
+              <TableHead>Anomaly Value</TableHead>
+              <TableHead>Delete</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
             {data.length > 0 ? (
               data.map((anomaly, index) => (
                 <TableRow key={index}>
-                  <TableCell>{anomaly.charger_id}</TableCell>
+                  <TableCell>
+                    <Link
+                      to={`/details/${anomaly.charger_id}`}
+                      className="text-black-600 hover:underline"
+                    >
+                    {anomaly.charger_id}
+                    </Link>
+                    </TableCell>
                   <TableCell>
                     {new Date(anomaly.timestamp).toLocaleString("de-DE", {
                       dateStyle: "short",
@@ -67,12 +92,22 @@ export default function AnomalyTable() {
                   </TableCell>
                   <TableCell>{anomaly.telemetry_type}</TableCell>
                   <TableCell>{anomaly.anomaly_type}</TableCell>
+                  <TableCell><span className="text-red-600 font-medium">{anomaly.anomaly_value}</span></TableCell>
+                  <TableCell>
+                    <button
+                      onClick={() => handleDelete(anomaly)}
+                      className="text-red-600 hover:text-red-800 font-bold cursor-pointer"
+                      title="delete"
+                    >
+                      ✕
+                    </button>
+                  </TableCell>
                 </TableRow>
               ))
             ) : (
               <TableRow>
                 <TableCell colSpan={4} className="text-center">
-                  Keine Daten gefunden.
+                  No data found.
                 </TableCell>
               </TableRow>
             )}
